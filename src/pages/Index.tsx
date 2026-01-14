c966c64549456ffbc87e5b5fe0958faeda74531e
Andrew-Cr7 <andrewfjdenham@gmail.com>
2026-01-14 16:58:25 +0000

    New website

---
+++ src/pages/Index.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import HowItWorks from "@/components/HowItWorks";
+import Story from "@/components/Story";
+import WaitlistCTA from "@/components/WaitlistCTA";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+      <main>
+        <Hero />
+        <Features />
+        <HowItWorks />
+        <Story />
+        <WaitlistCTA />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
