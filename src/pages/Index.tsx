54e016dba6aa446eb5e0deaa4875299755c33782
Andrew-Cr7 <andrewfjdenham@gmail.com>
2026-01-20 15:03:38 +0000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import HowItWorks from "@/components/HowItWorks";
 import Story from "@/components/Story";
-import FAQ from "@/components/FAQ";
+import FAQ from "@/components/FAQ.tsx";
 import WaitlistCTA from "@/components/WaitlistCTA";
 import Footer from "@/components/Footer";
 
