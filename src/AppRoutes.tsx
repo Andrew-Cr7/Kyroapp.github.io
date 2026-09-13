71b9cd46e920fc980a851507a78af93a6e1b9693
Andrew-Cr7 <andrewfjdenham@gmail.com>
2026-09-13 14:31:10 +0100

    Extract shared application routes for prerendering

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,102 @@
+import SeoConversionEnhancer from "@/components/SeoConversionEnhancer";
+import { Route, Routes } from "react-router-dom";
+
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import LondonGymDayPasses from "./pages/LondonGymDayPasses";
+import ForGyms from "./pages/ForGymsV2";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import {
+  CanTouristsUseGymsInLondon,
+  GymDayPassesUk,
+  HowToFindAGymWhileTravelling,
+} from "./pages/SeoContentPages";
+import {
+  AirportLayoverGymAccess,
+  BusinessTravelGymAccess,
+  FitnessWhileTravellingGuide,
+  GymAccessForDigitalNomads,
+  VisitorGymAccess,
+} from "./pages/SeoBatchPages";
+import {
+  CanUseGymWithoutMembership,
+  FindingAGymInANewCity,
+  FlexibleGymPassesForTravellers,
+  GymDayPassesExplained,
+  HowToFindGymsWithDayPasses,
+} from "./pages/SeoEvergreenPages";
+import {
+  HowToStayFitWhileTravelling,
+  ShortTermGymMembershipAlternatives,
+  TouristGymPass,
+} from "./pages/SeoNextPages";
+
+const AppRoutes = () => (
+  <>
+    <SeoConversionEnhancer />
+    <Routes>
+      <Route path="/" element={<Index />} />
+
+      <Route path="/london-gym-day-passes" element={<LondonGymDayPasses />} />
+      <Route path="/gym-day-passes-uk" element={<GymDayPassesUk />} />
+      <Route
+        path="/how-to-find-a-gym-while-travelling"
+        element={<HowToFindAGymWhileTravelling />}
+      />
+      <Route
+        path="/can-tourists-use-gyms-in-london"
+        element={<CanTouristsUseGymsInLondon />}
+      />
+      <Route
+        path="/fitness-while-travelling-guide"
+        element={<FitnessWhileTravellingGuide />}
+      />
+      <Route
+        path="/gym-access-for-digital-nomads"
+        element={<GymAccessForDigitalNomads />}
+      />
+      <Route
+        path="/business-travel-gym-access"
+        element={<BusinessTravelGymAccess />}
+      />
+      <Route path="/visitor-gym-access" element={<VisitorGymAccess />} />
+      <Route
+        path="/airport-layover-gym-access"
+        element={<AirportLayoverGymAccess />}
+      />
+      <Route
+        path="/can-you-use-a-gym-without-a-membership"
+        element={<CanUseGymWithoutMembership />}
+      />
+      <Route path="/gym-day-passes-explained" element={<GymDayPassesExplained />} />
+      <Route
+        path="/how-to-find-gyms-with-day-passes"
+        element={<HowToFindGymsWithDayPasses />}
+      />
+      <Route
+        path="/flexible-gym-passes-for-travellers"
+        element={<FlexibleGymPassesForTravellers />}
+      />
+      <Route
+        path="/finding-a-gym-in-a-new-city"
+        element={<FindingAGymInANewCity />}
+      />
+      <Route
+        path="/short-term-gym-membership-alternatives"
+        element={<ShortTermGymMembershipAlternatives />}
+      />
+      <Route path="/tourist-gym-pass" element={<TouristGymPass />} />
+      <Route
+        path="/how-to-stay-fit-while-travelling"
+        element={<HowToStayFitWhileTravelling />}
+      />
+
+      <Route path="/for-gyms" element={<ForGyms />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </>
+);
+
+export default AppRoutes;
